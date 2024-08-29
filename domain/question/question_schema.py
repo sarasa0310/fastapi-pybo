fa1148dd3eafd141bb282b3ffa86539260cce2f0
Jimmy <dev.sarasa.0310@gmail.com>
2024-08-29 16:29:26 +0900

    2-07 답변 등록까지 완료

--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -2,9 +2,13 @@ import datetime
 
 from pydantic import BaseModel
 
+from domain.answer.answer_schema import Answer
+
+
 # 질문 스키마
 class Question(BaseModel):
     id: int
     subject: str
     content: str
     create_date: datetime.datetime
+    answers: list[Answer] = []
